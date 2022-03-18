a94b699ca20196604e3d339b9018202c041f686b
Hong Phuc <hongphuc.dthp@gmail.com>
2022-03-18 23:32:02 +0700

    config Netlify deployment

--- svelte.config.js
+++ svelte.config.js
@@ -1,15 +1,8 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from "@sveltejs/adapter-netlify";
 
-/** @type {import('@sveltejs/kit').Config} */
-const config = {
-	kit: {
-		adapter: adapter(),
-
-		// Override http methods in the Todo forms
-		methodOverride: {
-			allowed: ['PATCH', 'DELETE']
-		}
-	}
-};
-
-export default config;
+export default {
+  kit: {
+    adapter: adapter(),
+    target: '#svelte',
+  }
+}
